0733ea43d7ec99eead0d2dc5dc0b3965fec65702
Johnny Wong <jwon145@cse.unsw.edu.au>
2011-05-04 12:34:25 +1000

    added addLetter helper function

--- dictionary.c
+++ dictionary.c
@@ -1,10 +1,21 @@
+#include <stdlib.h>
 #include "dictionary.h"
 #include "dictionary_type.h"
 
+// helper function prototypes
+dictLink addLetter(char letter);
 
-void initDict();
+static dictLink root;
 
-void insertWordDict(char *word);
+void initDict() {
+    root = NULL;
+}
+
+void insertWordDict(char *word) {
+    if (root == NULL) {
+        root = addLetter(*word);
+    }
+}
 
 void insertWordsDict(wordList words);
 
@@ -15,3 +26,16 @@ wordList completionsDict(char *word);
 void releaseDict();
 
 dictLink getRootDict(); 
+
+// helper functions
+
+dictLink addLetter(char letter) {
+    dictLink new = malloc(sizeof(struct dictEdge));
+
+    new->thisChar = letter;
+    new->isTerminal = False;
+    new->child = NULL;
+    new->sibling = NULL;
+
+    return new;
+}
