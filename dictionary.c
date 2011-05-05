4e5d12a59399bfc3ae96c45d7f3c9052ff009a6d
Johnny Wong <jwon145@cse.unsw.edu.au>
2011-05-05 22:54:22 +1000

    implemented initDict and insertWordDict

--- dictionary.c
+++ dictionary.c
@@ -1,21 +1,39 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "dictionary.h"
 #include "dictionary_type.h"
 
 // helper function prototypes
 dictLink addLetter(char letter);
+dictLink ptrToSibling(dictLink currSibling, char letter);
+void printAll(dictLink curr);
 
 static dictLink root;
 
-void initDict() {
+void initDict() {/*{{{*/
     root = NULL;
-}
+}/*}}}*/
 
-void insertWordDict(char *word) {
+void insertWordDict(char *word) {/*{{{*/
+    dictLink curr;
+    char letter = *(word++);
+
     if (root == NULL) {
-        root = addLetter(*word);
+        root = addLetter(letter);
+        curr = root;
+    } else {
+        curr = ptrToSibling(curr, letter);
     }
-}
+
+    while (curr != NULL && (letter = *(word++)) != '\0') {
+        if (curr->child == NULL) {
+            curr->child = addLetter(letter);
+            curr = curr->child;
+        } else {
+            curr = ptrToSibling(curr->child, letter);
+        }
+    }
+}/*}}}*/
 
 void insertWordsDict(wordList words);
 
@@ -29,7 +47,7 @@ dictLink getRootDict();
 
 // helper functions
 
-dictLink addLetter(char letter) {
+dictLink addLetter(char letter) {/*{{{*/
     dictLink new = malloc(sizeof(struct dictEdge));
 
     new->thisChar = letter;
@@ -38,4 +56,27 @@ dictLink addLetter(char letter) {
     new->sibling = NULL;
 
     return new;
-}
+}/*}}}*/
+
+dictLink ptrToSibling(dictLink currSibling, char letter) {/*{{{*/
+    if (currSibling->thisChar == letter) {
+        return currSibling;
+    }
+    while (currSibling->sibling != NULL) {
+        if (currSibling->thisChar == letter) {
+            return currSibling->sibling;
+        }
+        currSibling = currSibling->sibling;
+    }
+    currSibling->sibling = addLetter(letter);
+    return currSibling->sibling;
+}/*}}}*/
+
+void printAll(dictLink curr) {/*{{{*/
+    if (curr != NULL) {
+        printf("%c\n", curr->thisChar);
+        printAll(curr->child);
+        printAll(curr->sibling);
+    }
+}/*}}}*/
+
